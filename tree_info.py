fdbc5f5c075cc8a4f19eae818afdf4953461bfa1
Ping Wu <wpwupingwp@outlook.com>
2023-11-09 15:42:22 +0800

    only use one word

--- tree_info.py
+++ tree_info.py
@@ -9,11 +9,9 @@ from global_vars import log
 pattern = re.compile(r'\W')
 
 
-def get_taxon_list() -> (set, set, set, set):
+def get_taxon_list() -> (set, set, set):
     # from barcodefinder
-    global species_set, genus_set, family_set, order_set
-    with open('data/species.csv', 'r') as _:
-        species_set = set(_.read().strip().split(','))
+    global genus_set, family_set, order_set
     with open('data/genus.csv', 'r') as _:
         genus_set = set(_.read().strip().split(','))
     with open('data/other_families.csv', 'r') as _:
@@ -24,35 +22,27 @@ def get_taxon_list() -> (set, set, set, set):
         order_set = set(_.read().strip().split(','))
     with open('data/other_orders.csv', 'r') as _:
         order_set.update(_.read().strip().split(','))
-    return species_set, genus_set, family_set, order_set
+    return genus_set, family_set, order_set
 
 
-def get_name_candidate(words: str) -> (set, set):
-    word_list = [i for i in re.split(pattern, words) if i]
-    species_alike = set()
-    lineage_alike = set()
-    for i in range(len(word_list)-1):
-        if word_list[i][0].isupper():
-            if word_list[i+1][0].islower():
-                species_alike.add(' '.join(word_list[i:i+2]))
-            else:
-                lineage_alike.add(word_list[i])
-    return species_alike, lineage_alike
+def get_name_candidate(record: Result) -> set:
+    word_set = set()
+    for content in (record.title, record.abstract):
+        for word in re.split(pattern, content):
+            if word and len(word) > 1 and word[0].isupper():
+                word_set.add(word)
+    return word_set
 
 
-def get_lineage(species_alike: set, lineage_alike: set) -> str:
-    # species first, then genus, family or order
-    # but return genus rather than species
-    species_name = species_alike & species_set
-    if species_name:
-        return species_name.pop().split(' ')[0]
-    genus_name = lineage_alike & genus_set
+def get_lineage(word_set: set) -> str:
+    # first genus, family or order
+    genus_name = word_set & genus_set
     if genus_name:
         return genus_name.pop()
-    family_name = lineage_alike & family_set
+    family_name = word_set & family_set
     if family_name:
         return family_name.pop()
-    order_name = lineage_alike & order_set
+    order_name = word_set & order_set
     if order_name:
         return order_name.pop()
     return ''
@@ -60,20 +50,18 @@ def get_lineage(species_alike: set, lineage_alike: set) -> str:
 
 def assign_lineage(record: Result):
     # todo: test
-    words = ' '.join([record.title, record.abstract]).strip()
-    species_alike, lineage_alike = get_name_candidate(words)
-    print(species_alike, lineage_alike)
-    record.lineage = get_lineage(species_alike, lineage_alike)
+    word_set = get_name_candidate(record)
+    record.lineage = get_lineage(word_set)
     if not record.lineage:
-        log.warning(f'{record.doi} cannot find lineage')
-    else:
-        log.info(f'Assign {record.lineage} to {record.doi}')
+        print(word_set, record.abstract)
     return record
 
 
 def main():
     get_taxon_list()
     file_list = list(Path('result').glob('*.result.json.new'))
+    total = 0
+    assigned = 0
     for result_json in file_list:
         old_records = json.load(open(result_json, 'r'))
         new_records = list()
@@ -82,12 +70,19 @@ def main():
             record = Result(**raw_record)
             if not record.tree_files:
                 continue
+            total += 1
             record = assign_lineage(record)
+            if not record.lineage:
+                log.warning(f'{record.doi} cannot find lineage')
+            else:
+                assigned += 1
+                log.info(f'Assign {record.lineage} to {record.doi}')
             with open(new_result_file, 'w') as f:
                 pass
                 # json.dump(new_records, f)
             # break
         # break
+    print(total, assigned)
     return
 
 
