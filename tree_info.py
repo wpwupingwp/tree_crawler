4210d98f4e5b84c365b8092e105bc5b58a2c6e2b
Ping Wu <wpwupingwp@outlook.com>
2023-11-09 12:34:58 +0800

    fix get_name_candidate

--- tree_info.py
+++ tree_info.py
@@ -28,12 +28,12 @@ def get_taxon_list() -> (set, set, set, set):
 
 
 def get_name_candidate(words: str) -> (set, set):
-    word_list = re.split(pattern, words)
+    word_list = [i for i in re.split(pattern, words) if i]
     species_alike = set()
     lineage_alike = set()
     for i in range(len(word_list)-1):
-        if word_list[i].isupper():
-            if word_list[i+1].islower():
+        if word_list[i][0].isupper():
+            if word_list[i+1][0].islower():
                 species_alike.add(' '.join(word_list[i:i+2]))
             else:
                 lineage_alike.add(word_list[i])
@@ -60,9 +60,14 @@ def get_lineage(species_alike: set, lineage_alike: set) -> str:
 
 def assign_lineage(record: Result):
     # todo: test
-    words = ' '.join([record.title, record.abstract])
+    words = ' '.join([record.title, record.abstract]).strip()
     species_alike, lineage_alike = get_name_candidate(words)
+    print(species_alike, lineage_alike)
     record.lineage = get_lineage(species_alike, lineage_alike)
+    if not record.lineage:
+        log.warning(f'{record.doi} cannot find lineage')
+    else:
+        log.info(f'Assign {record.lineage} to {record.doi}')
     return record
 
 
@@ -71,14 +76,16 @@ def main():
     file_list = list(Path('result').glob('*.result.json.new'))
     for result_json in file_list:
         old_records = json.load(open(result_json, 'r'))
+        new_records = list()
         new_result_file = result_json.with_suffix('.json.new2')
         for raw_record in old_records:
             record = Result(**raw_record)
+            if not record.tree_files:
+                continue
             record = assign_lineage(record)
-            print(record)
-            with open(result_json, 'w') as f:
-                json.dump(old_records, f)
-            log.info(f'{record.identifier} lineage assigned')
+            with open(new_result_file, 'w') as f:
+                pass
+                # json.dump(new_records, f)
             # break
         # break
     return
